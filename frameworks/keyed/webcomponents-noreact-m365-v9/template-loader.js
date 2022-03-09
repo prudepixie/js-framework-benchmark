5aa23aae89f68ef635970264a94d05775936f626
nicholasrice <nicholasrice@users.noreply.github.com>
2022-03-09 10:50:01 -0800

    adding template minifier

---
+++ frameworks/keyed/webcomponents-noreact-m365-v9/template-loader.js
@@ -0,0 +1,15 @@
+const minify = require('minify-html-literals');
+module.exports = function (source) {
+    try {
+
+    const minified = minify.minifyHTMLLiterals(source);
+
+    if (minified && minified.code) {
+        return minified.code;
+    }
+
+    return source;
+    } catch(e) {
+        return source;
+    }
+}
--- frameworks/keyed/webcomponents-noreact-m365-v9/webpack.config.js
+++ frameworks/keyed/webcomponents-noreact-m365-v9/webpack.config.js
@@ -1,42 +1,48 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
 
-module.exports = function(env, { mode }) {
-  const production = mode === 'production';
+module.exports = function (env, { mode }) {
+  const production = mode === "production";
   return {
-    mode: production ? 'production' : 'development',
-    devtool: production ? 'source-map' : 'inline-source-map',
+    mode: production ? "production" : "development",
+    devtool: production ? "source-map" : "inline-source-map",
     entry: {
-      app: ['./src/main.ts']
+      app: ["./src/main.ts"],
     },
     output: {
-      filename: 'bundle.js'
+      filename: "bundle.js",
     },
     resolve: {
-      extensions: ['.ts', '.js'],
-      modules: ['src', 'node_modules']
+      extensions: [".ts", ".js"],
+      modules: ["src", "node_modules"],
     },
     devServer: {
       port: 9000,
       historyApiFallback: true,
       writeToDisk: true,
       open: !process.env.CI,
-      lazy: false
+      lazy: false,
     },
-    plugins: [
-      new CleanWebpackPlugin()
-    ],
+    plugins: [new CleanWebpackPlugin()],
     module: {
       rules: [
+        {
+          test: /\.js$/i,
+          use: [{ loader: path.resolve(__dirname, "./template-loader.js") }],
+        },
         {
           test: /\.ts$/i,
           use: [
             {
-              loader: 'ts-loader'
-            }
+              loader: path.resolve(__dirname, "./template-loader.js"),
+            },
+            {
+              loader: "ts-loader",
+            },
           ],
-          exclude: /node_modules/
-        }
-      ]
-    }
-  }
-}
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  };
+};
